d6eb7e6175fb7804a597cafccb906222c117b9b8
G4b0rDev <G4b0rDev@users.noreply.github.com>
2024-11-09 19:35:00 +0000

    style(lint): apply auto-fixes from linting

--- resources/js/markdown-cheatsheet.js
+++ resources/js/markdown-cheatsheet.js
@@ -1,10 +1,10 @@
-import '../css/markdown-cheatsheet.css';
+import '../css/markdown-cheatsheet.css'
 
-import hljs from 'highlight.js/lib/core';
+import hljs from 'highlight.js/lib/core'
 import markdown from 'highlight.js/lib/languages/markdown'
 
-hljs.registerLanguage('markdown', markdown);
+hljs.registerLanguage('markdown', markdown)
 
 document.querySelectorAll('.language-markdown').forEach((block) => {
-    hljs.highlightElement(block, { language: 'markdown', ignoreIllegals: true });
-});
+  hljs.highlightElement(block, { language: 'markdown', ignoreIllegals: true })
+})
--- src/FilamentMarkdownEditorCheatsheetServiceProvider.php
+++ src/FilamentMarkdownEditorCheatsheetServiceProvider.php
@@ -53,4 +53,3 @@ class FilamentMarkdownEditorCheatsheetServiceProvider extends PackageServiceProv
         ], 'markdown-cheatsheet');
     }
 }
-
